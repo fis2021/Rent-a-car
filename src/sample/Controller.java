dfefbac68bbad1215d94de2d0aee822ad3234e97
Șerban Radu <r.serbannn@gmail.com>
2021-03-20 15:33:24 +0200

    Window 1080x720 cu exit button functional

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private ImageView exit;
+
+    @FXML
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        exit.setOnMouseClicked(event -> {
+            System.exit(0);
+        });
+    }
 }
