afd6ba41770dcb7cc558a3ee6ae9263cb790d38e
Șerban Radu <r.serbannn@gmail.com>
2021-03-20 18:01:30 +0200

    culori diferite

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 
 import java.net.URL;
@@ -14,11 +15,13 @@ public class Controller implements Initializable {
     private ImageView exit;
 
     @FXML
+    private AnchorPane pane1, pane2;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         exit.setOnMouseClicked(event -> {
             System.exit(0);
         });
+
     }
 }
